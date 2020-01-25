8dcc73ee6c2308e057e8ff72e9a6e808060fde43
Tiffany Jalovec <carr.tiffany@gmail.com>
2020-01-24 18:40:11 -0700

    exited table

--- database/schema.sql
+++ database/schema.sql
@@ -5,7 +5,7 @@ CREATE DATABASE DibsOnDinnerPhotos;
 USE DibsOnDinnerPhotos;
 
 CREATE TABLE photos (
-  id int NOT NULL AUTO_INCREMENT,
-  url text NOT NULL,
-  restaurant_id int NOT NULL,
+  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  image text NOT NULL,
+  restaurant_id int NOT NULL
 );
